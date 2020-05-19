78ab9cd9d23858d1c52aa66e073b451512344fda
Unicom <tyyz614@163.com>
2020-05-19 15:29:04 +0800

    修复targetNamespace

--- src/main/java/com/javatest/webservice/server/impl/JAXTestServiceImpl.java
+++ src/main/java/com/javatest/webservice/server/impl/JAXTestServiceImpl.java
@@ -12,7 +12,9 @@ import javax.xml.ws.Endpoint;
 /**
  * webservice服务端实现类。注意，如果使用面向接口，那么在此实现类中必须通过endpointInterface指定服务端接口类
  */
-@WebService(targetNamespace = "impl.server.webservice.javatest.com",endpointInterface = "com.javatest.webservice.server.JAXTestService")
+@WebService(targetNamespace = "http://impl.server.webservice.javatest.com/",    // 命名空间一般为包名的倒置
+        endpointInterface = "com.javatest.webservice.server.JAXTestService"
+)
 @Service
 public class JAXTestServiceImpl implements JAXTestService {
 
@@ -27,7 +29,7 @@ public class JAXTestServiceImpl implements JAXTestService {
     // 发布服务
     public static void main(String[] args) {
         // 指定服务url
-            String url = "http://localhost:9010/javatest/webservice";
+            String url = "http://localhost:9010/javatest/webservice/getJAX";
         // 指定服务实现类
         JAXTestService server = new JAXTestServiceImpl();
         // 通过Endpoint发布服务
