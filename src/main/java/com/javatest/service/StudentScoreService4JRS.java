ae5bb6a5bf45fb795ade046ccdd11f4347cecace
Unicom <tyyz614@163.com>
2020-07-01 17:39:21 +0800

    cxf restful接口（完成）

--- src/main/java/com/javatest/service/StudentScoreService4JRS.java
+++ src/main/java/com/javatest/service/StudentScoreService4JRS.java
@@ -13,14 +13,16 @@ import java.util.List;
 @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 public interface StudentScoreService4JRS {
 
-    @POST                                   // 请求格式：有GET\POST\PUT\DELETE等
+    //TODO 如果改用POST，id以json字符串传入时一直报错“Cannot deserialize instance of `java.lang.Long` out of START_OBJECT token”，
+    // 但如果以一个对象传入，后面用一个对象接收则不会报错，原因未明
+    @GET                                   // 请求格式：有GET\POST\PUT\DELETE等
     @Produces(MediaType.APPLICATION_XML)    // 返回xml格式的数据
     @Consumes(MediaType.APPLICATION_JSON)   // 请求的格式规定为json
-    @Path("/xml/findOne/")  // 指定请求的路径
-    StudentScore4JRS selectByPrimaryKey(Long id);
+    @Path("/xml/findOne/{id}")  // 指定请求的路径
+    StudentScore4JRS selectByPrimaryKey(@PathParam("id") Long id);
 
-    @GET
+    @POST
     @Produces(MediaType.APPLICATION_JSON)   // 返回JSON格式的数据
-    @Path("/json/findAll/")
+    @Path("/json/findAll")
     List<StudentScore4JRS> queryStudentScore(StudentScore4JRS studentScore);
 }
