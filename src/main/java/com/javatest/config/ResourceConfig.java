fe7aefa9cb7a6458a65c45277022c508d9254f73
Unicom <tyyz614@163.com>
2020-09-07 17:34:35 +0800

    新增quartz的controller，修复请求的格式化问题（但似乎没用）

--- src/main/java/com/javatest/config/ResourceConfig.java
+++ src/main/java/com/javatest/config/ResourceConfig.java
@@ -1,16 +1,25 @@
 package com.javatest.config;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
-@Configuration
+/**
+ * @author azure
+ * @desp 重写转换器，但由于重写会使默认的配置失效，会出现很多奇奇怪怪的问题，所以废弃不用。
+ */
+//@Configuration
 public class ResourceConfig extends WebMvcConfigurationSupport {
 
     /**
@@ -31,11 +40,50 @@ public class ResourceConfig extends WebMvcConfigurationSupport {
     }
 
     /**
+     * 新增json转换器，解决因重写转换器导致@RequestBody解析json失败的问题
+     * 但相反的，如果参数不是json格式，会导致报错
+     */
+    @Bean
+    public FastJsonHttpMessageConverter fastJsonConverter() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        // 设置处理的类型
+        List<MediaType> supportedMediaTypes = new ArrayList<>();
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+        supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+        supportedMediaTypes.add(MediaType.APPLICATION_PDF);
+        supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_XML);
+        supportedMediaTypes.add(MediaType.IMAGE_GIF);
+        supportedMediaTypes.add(MediaType.IMAGE_JPEG);
+        supportedMediaTypes.add(MediaType.IMAGE_PNG);
+        supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
+        supportedMediaTypes.add(MediaType.TEXT_HTML);
+        supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
+        supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+        supportedMediaTypes.add(MediaType.TEXT_XML);
+        fastConverter.setSupportedMediaTypes(supportedMediaTypes);
+        //修改配置返回内容的过滤
+//        fastJsonConfig.setSerializerFeatures(
+//                SerializerFeature.DisableCircularReferenceDetect,
+//                SerializerFeature.WriteMapNullValue,
+//                SerializerFeature.WriteNullStringAsEmpty
+//        );
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        return fastConverter;
+    }
+
+    /**
      * 添加新的转换器
      */
     @Override
     protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         super.configureMessageConverters(converters);
         converters.add(responseBodyConverter());
+        converters.add(fastJsonConverter());
     }
 }
---
+++ src/main/java/com/javatest/controller/QuartzController.java
@@ -0,0 +1,63 @@
+package com.javatest.controller;
+
+import com.javatest.domain.Schedule;
+import com.javatest.enums.ScheduleStatusEnum;
+import com.javatest.schedule.quartz.projectSample.service.QuartzManager;
+import com.javatest.service.ScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/quartz")
+public class QuartzController {
+
+    @Autowired
+    private QuartzManager quartzManager;
+    @Autowired
+    private ScheduleService scheduleService;
+
+    @PostMapping("/addSchedule")
+    public void addSchedule(@RequestBody Schedule schedule) {
+        scheduleService.insertSelective(schedule);
+        quartzManager.addJob(schedule);
+    }
+
+    @PutMapping("/modifySchedule")
+    public void modifySchedule(@RequestBody Schedule schedule) {
+        scheduleService.updateByPrimaryKeySelective(schedule);
+        Map<String,Object> map = new HashMap<>();
+        map.put("schedule",schedule);
+        quartzManager.modifyJob(schedule.getTaskId(),schedule.getCronExpression(),map);
+    }
+
+    @PostMapping("/pauseSchedule")
+    public void pauseSchedule(int id) {
+        Schedule schedule = scheduleService.selectByPrimaryKey(id);
+        schedule.setStatus(ScheduleStatusEnum.PAUSE.getStatus());
+        scheduleService.updateByPrimaryKeySelective(schedule);
+        quartzManager.pauseJob(schedule.getTaskId());
+    }
+
+    @PostMapping("/resumeSchedule")
+    public void resumeSchedule(int id) {
+        Schedule schedule = scheduleService.selectByPrimaryKey(id);
+        schedule.setStatus(ScheduleStatusEnum.RUNNABLE.getStatus());
+        scheduleService.updateByPrimaryKeySelective(schedule);
+        quartzManager.resumeJob(schedule.getTaskId());
+    }
+
+    @PostMapping("/delSchedule")
+    public void delSchedule(int id) {
+        Schedule schedule = scheduleService.selectByPrimaryKey(id);
+        quartzManager.removeJob(schedule.getTaskId());
+        scheduleService.deleteByPrimaryKey(id);
+    }
+
+    @GetMapping("/shutDownSchedule")
+    public void shutDownSchedule() {
+        quartzManager.shutdownSchedule();
+    }
+}
